54556f807ae4689ba18a8c5bbbcdc39bc176c6b2
Sam Bauch <sbauch@gmail.com>
2022-10-07 01:28:00 -0400

    feat: 🎸 Adds `rpc` indexer (#3)
    The RPC indexer is a fallback for fetching metadata directly from ERC721#tokenURI

--- src/indexers/index.ts
+++ src/indexers/index.ts
@@ -1,6 +1,7 @@
 import alchemy from './alchemy';
 import center from './center';
 import infura from './infura';
+import rpc from './rpc';
 import zora from './zora';
 
-export { alchemy, center, infura, zora };
+export { alchemy, center, infura, rpc, zora };
